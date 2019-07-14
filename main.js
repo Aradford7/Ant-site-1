bdb7d1de1fd9afb3d265d07535b598fc0fbcbb99
Ashley Radford <aradford7@outlook.com>
2019-07-13 17:30:20 -0700

    test

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 new hoverEffect({
     parent: document.querySelector('.distortion'),
     intensity: 0.2,
-    image1: '/Hoveranimation/Anthony-site/images/Anthony1.jpg',
-    image2: '/Hoveranimation/Anthony-site/images/Anthony1.jpg',
-    displacementImage: '/Hoveranimation/Anthony-site/hover-effect/displacement.png'
+    image1: '/Anthony-site/images/Anthony1.jpg',
+    image2: '/Anthony-site/images/Anthony1.jpg',
+    displacementImage: '/Anthony-site/hover-effect/displacement.png'
 });
