1796d5016418e8cd33a4d2150a0004133b0b4bd9
Ashley Radford <aradford7@outlook.com>
2019-07-14 12:19:15 -0700

    add speed

--- main.js
+++ main.js
@@ -3,5 +3,7 @@ new hoverEffect({
     intensity: 0.2,
     image1: '/images/Anthony1.jpg',
     image2: '/images/Anthony2.jpg',
+    speedIn: 2,
+    speedOut: 5,
     displacementImage: '/hover-effect/displacement.png'
 });
