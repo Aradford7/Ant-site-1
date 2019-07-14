48bcb2a9af13ab52059d2545a708ea23bbba1841
Ashley Radford <aradford7@outlook.com>
2019-07-13 17:43:31 -0700

    fix second pic

--- main.js
+++ main.js
@@ -2,6 +2,6 @@ new hoverEffect({
     parent: document.querySelector('.distortion'),
     intensity: 0.2,
     image1: '/images/Anthony1.jpg',
-    image2: '/images/Anthony1.jpg',
+    image2: '/images/Anthony2.jpg',
     displacementImage: '/hover-effect/displacement.png'
 });
