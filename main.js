96bacd4d7a660b83686e69c5e444ed5268cc2028
Ashley Radford <aradford7@outlook.com>
2019-07-13 17:24:27 -0700

    test

--- main.js
+++ main.js
@@ -3,5 +3,5 @@ new hoverEffect({
     intensity: 0.2,
     image1: '/Hoveranimation/Anthony-site/images/Anthony1.jpg',
     image2: '/Hoveranimation/Anthony-site/images/Anthony1.jpg',
-    displacementImage: '../hover-effect/displacement.png'
+    displacementImage: '/Hoveranimation/Anthony-site/hover-effect/displacement.png'
 });
