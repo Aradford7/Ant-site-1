16aad266fdac189cec9639a82647eea5700d6fc5
Ashley Radford <aradford7@outlook.com>
2019-07-13 17:41:10 -0700

    asdad

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 new hoverEffect({
     parent: document.querySelector('.distortion'),
     intensity: 0.2,
-    image1: '/Anthony-site/images/Anthony1.jpg',
-    image2: '/Anthony-site/images/Anthony1.jpg',
-    displacementImage: '/Anthony-site/hover-effect/displacement.png'
+    image1: '/images/Anthony1.jpg',
+    image2: '/images/Anthony1.jpg',
+    displacementImage: '/hover-effect/displacement.png'
 });
